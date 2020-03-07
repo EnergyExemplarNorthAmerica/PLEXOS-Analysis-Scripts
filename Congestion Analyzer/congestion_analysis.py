485ef53fe56c99abb7dc99c480e8b36ecea60d90
Steven Broad <steven.broad@energyexemplar.com>
2020-03-06 21:40:29 -0800

    Added some comments to the script

--- Congestion Analyzer/congestion_analysis.py
+++ Congestion Analyzer/congestion_analysis.py
@@ -32,16 +32,19 @@ from EEUTILITY.Enums import *
 
 def main(FB, TB, TS, ptdf_data, plexos_db = '', plexos_sol = '', db=None, sol=None):
 
+    # Initialize timer and file io
     start_time = time.time()
     Line = '{}_{}'.format(FB,TB)
     temp_file = '{}_sol.csv'.format(Line)
     results_folder = './Results {}'.format(Line)
+    results_excel = '{}.xlsx'.format(Line)
+
+    # create the relevant results folder
     os.makedirs(results_folder, exist_ok=True)
 
+    # Load PLEXOS Input and Output data
     if db is None:
         db = DatabaseCore()
-
-        # Enter the path for your Database:
         db.Connection(plexos_db)
         print('Loaded PLEXOS input:', time.time() - start_time)
 
@@ -50,119 +53,107 @@ def main(FB, TB, TS, ptdf_data, plexos_db = '', plexos_sol = '', db=None, sol=No
         sol.Connection(plexos_sol)
         print('Loaded PLEXOS solution:', time.time() - start_time)
 
+    # Before we load the PTDF data, let's first find which data we actually want to utilize
+    #   The PTDF data file is produced from a different PLEXOS Analysis script.
     with open(ptdf_data) as infile:
-        idxs = [idx for idx, from_bus, to_bus in zip(infile.readline().split(','),infile.readline().split(','),infile.readline().split(',')) if FB in from_bus and TB in to_bus]
+        # read the first 3 lines of the file
+        #   line1 is the columns header
+        #   line2 is a list of "from" busses
+        #   line3 is a list of "to" busses
+        line1, line2, line3 = infile.readline(), infile.readline(), infile.readline()
+
+        # find only those columns that have the right from and to bus pairs
+        idxs = [idx for idx, from_bus, to_bus in zip(line1.split(','), line2.split(','), line3.split(',')) if FB in from_bus and TB in to_bus]
+
         print('Computed required PTDF columns:', time.time() - start_time)
-
-    df = pd.read_csv(ptdf_data, usecols=['0'] + idxs, low_memory=False, skiprows=[1,2])
-    df.rename(columns = {'0':'child_name'}, inplace=True)
-
+    
+    # Pull only those data that are related to this from-to pair
+    ptdf_df = pd.read_csv(ptdf_data, usecols=['0'] + idxs, low_memory=False, skiprows=[1,2])
+    ptdf_df.rename(columns = {'0':'child_name'}, inplace=True)
     print('Loaded PTDF data:', time.time() - start_time)
 
+    # Pull a query from the PLEXOS data into a CSV file
+    #   below is the signature of the API method
     '''
-    Data.iloc[[0,1]] = Data.iloc[[0,1]].applymap(str)
-
-    for idx1, column in enumerate(Data.iloc[0]):
-        
-        for idx2, column1 in enumerate(Data.iloc[1]):
-            if ((column[:len(FB)] == FB) and (column1[:len(TB)] == TB) and (idx1 == idx2)):
-                df = Data.iloc[:, idx1]
-                df = pd.concat([Data.iloc[:,1], df], axis=1)  
-                break
-            else:
-                continue
-
-        continue
-        break
+    Boolean QueryToCSV(
+            String strCSVFile,
+            Boolean bAppendToFile,
+            SimulationPhaseEnum SimulationPhaseId,
+            CollectionEnum CollectionId,
+            String ParentName,
+            String ChildName,
+            PeriodEnum PeriodTypeId,
+            SeriesTypeEnum SeriesTypeId,
+            String PropertyList[ = None],
+            Object DateFrom[ = None],
+            Object DateTo[ = None],
+            String TimesliceList[ = None],
+            String SampleList[ = None],
+            String ModelName[ = None],
+            AggregationEnum AggregationType[ = None],
+            String Category[ = None],
+            String Separator[ = ,]
+            )
     '''
-
-    try:
-        '''
-        Boolean QueryToCSV(
-                String strCSVFile,
-                Boolean bAppendToFile,
-                SimulationPhaseEnum SimulationPhaseId,
-                CollectionEnum CollectionId,
-                String ParentName,
-                String ChildName,
-                PeriodEnum PeriodTypeId,
-                SeriesTypeEnum SeriesTypeId,
-                String PropertyList[ = None],
-                Object DateFrom[ = None],
-                Object DateTo[ = None],
-                String TimesliceList[ = None],
-                String SampleList[ = None],
-                String ModelName[ = None],
-                AggregationEnum AggregationType[ = None],
-                String Category[ = None],
-                String Separator[ = ,]
-                )
-        '''
-        sol.QueryToCSV(temp_file, False, \
-                        SimulationPhaseEnum.STSchedule, \
-                        CollectionEnum.SystemNodes, \
-                        '', \
-                        '', \
-                        PeriodEnum.Interval, \
-                        SeriesTypeEnum.Periods, \
-                        '22')
-        df8 = pd.read_csv(temp_file)
-        os.remove(temp_file)
-        df8 = df8.drop(df8.columns[[0,1,2,3,4,5,6,7,8,9,11,13,14,15,16,17,18,19]], axis = 1)
-        print('Queried Net Injections:', time.time() - start_time)
+    sol.QueryToCSV(temp_file, False, \
+                    SimulationPhaseEnum.STSchedule, \
+                    CollectionEnum.SystemNodes, \
+                    '', \
+                    '', \
+                    PeriodEnum.Interval, \
+                    SeriesTypeEnum.Periods, \
+                    '22')
+    injection_df = pd.read_csv(temp_file)
+    os.remove(temp_file)
+    injection_df = injection_df.drop(injection_df.columns[[0,1,2,3,4,5,6,7,8,9,11,13,14,15,16,17,18,19]], axis = 1)
+    print('Queried Net Injections:', time.time() - start_time)
         
-    except:
-        pass
-
-    writer = pd.ExcelWriter(os.path.join(results_folder, '{}.xlsx.'.format(Line)), engine='xlsxwriter')
-    df1 = pd.DataFrame(columns=['child_name',Line])
-    for col in [col for col in df if not col == 'index']:
-        df1['child_name'] = df['child_name']
-        df1[Line] = df[col]
+    # join injections to ptdfs
+    temp_injection_df = pd.DataFrame(columns=['child_name',Line])
+    for col in [col for col in ptdf_df if not col == 'index']:
+        temp_injection_df['child_name'] = ptdf_df['child_name']
+        temp_injection_df[Line] = ptdf_df[col]
 
 
-    df['child_name'] = df.applymap(str)
-    df8['child_name'] = df8.applymap(str)
+    ptdf_df['child_name'] = ptdf_df.applymap(str)
+    injection_df['child_name'] = injection_df.applymap(str)
 
-    df = df.merge(df8, on='child_name')
+    ptdf_df = ptdf_df.merge(injection_df, on='child_name')
 
-    df8 = df8.merge(df1, on='child_name')
+    injection_df = injection_df.merge(temp_injection_df, on='child_name')
 
+    # begin calculations of shift factors and impacts
     Mult = pd.DataFrame()
-    SF = pd.DataFrame()
+    Mult[Line] = injection_df.iloc[:,0]
 
-    Mult[Line] = df8.iloc[:,0]
+    for z in range(2, len(injection_df.columns)-1):
+        row = injection_df.columns[z]
+        injection_df[row] = injection_df[row].astype(float)
+        injection_df[Line] = injection_df[Line].astype(float)
+        injection_df = injection_df.dropna()
+        Mult[row] = injection_df[row] * injection_df[Line]
+    
+    Mult['Shift Factor'] = injection_df[Line]
 
-    for z in range (2, len(df8.columns)-1):
-        row = df8.columns[z]
-        df8[row] = df8[row].astype(float)
-        df8[Line] = df8[Line].astype(float)
-        df8 = df8.dropna()
-        Mult[row] = df8[row] * df8[Line]
-        
+    # drop columns that are not needed
+    # we only need the columns for the transmission path, the timestamp we are querying, and the Shift Factor
+    Mult = Mult.drop([col for col in Mult.columns if col not in [Line, TS, 'Shift Factor']], axis = 1)
 
-    Mult['Shift Factor'] = df8[Line]
-
-    # Taking the Timestamp of interest from the user:
-    # print('The First Time Stamp is :{}, The Last Time Stamp is :{}.'.format(Mult.columns[1],Mult.columns[len(Mult.columns)-2]))
-    #Timestamp = str(input("Enter the Time Stamp with the Same Format Above :"))
-
-    for col in Mult.columns:
-        if col != Line and col != TS and col != 'Shift Factor':
-            Mult = Mult.drop([col], axis = 1)
-            
+    # we need another data frame for shift factor analysis: we will sort them differently
     SF = Mult        
+    Mult = Mult.sort_values([TS]) # sorting on injections
+    SF = SF.sort_values(['Shift Factor']) # sorting by shift factors
 
-    Mult = Mult.sort_values([TS])
-    SF = SF.sort_values(['Shift Factor'])
-
+    # Part of our impact analysis is to focus on those buses / generators that are most impactful
+    #   in relieving or causing congestion
     Merge = Mult.head(50).merge(SF.head(50), on = Line, how = 'outer')
     Merge1 = Mult.tail(50).merge(SF.tail(50), on = Line, how = 'outer')
     Merge = Merge.dropna()
     Merge1 = Merge1.dropna()
     Merge = pd.concat([Merge,Merge1], axis=0)
 
-    df9 = df8
+    # this analysis will relate the generators to the buses in question
+    df9 = injection_df
     df9.rename(columns = {'child_name':'Node'}, inplace=True)
 
     print('Load gen-bus mapping', time.time() - start_time)
@@ -185,7 +176,7 @@ def main(FB, TB, TS, ptdf_data, plexos_db = '', plexos_sol = '', db=None, sol=No
         if col != 'Node' and col != TS and col != Line and col != 'Generator':
             mem = mem.drop([col], axis = 1)
                 
-    
+    # query genreation to a temporary file
     print('Loading generation', time.time() - start_time)
     sol.QueryToCSV(temp_file, True, \
                     SimulationPhaseEnum.STSchedule, \
@@ -232,6 +223,7 @@ def main(FB, TB, TS, ptdf_data, plexos_db = '', plexos_sol = '', db=None, sol=No
     mem = mem.dropna()
     
     print('Preparing to write results', time.time() - start_time)
+    writer = pd.ExcelWriter(os.path.join(results_folder, '{}.xlsx.'.format(Line)), engine='xlsxwriter')
     Mult.to_excel(writer, sheet_name='NI x SF')
     SF.to_excel(writer, sheet_name='SF')
     mem.to_excel(writer, sheet_name='Avg Cost')
@@ -242,17 +234,27 @@ def main(FB, TB, TS, ptdf_data, plexos_db = '', plexos_sol = '', db=None, sol=No
 
     return db, sol
 
+
+# run this script
 if __name__ == '__main__':
     plexos_cxn = None
     plexos_sol = None
 
-    plexos_db = sys.argv[1]
-    ptdf_file = sys.argv[2]
-    sol_file = sys.argv[3]
+    plexos_db = sys.argv[1] # first argument is the plexos input db filename
+    ptdf_file = sys.argv[2] # second argument is the ptdf file produced by a different script
+    sol_file = sys.argv[3]  # third argument is the plexos solution file
 
+    # arguments past the fourth should appear in triples
+    #   4th: from_bus
+    #   5th: to_bus
+    #   6th: timestamp
     for from_bus, to_bus, timestamp in zip(sys.argv[4::3], sys.argv[5::3], sys.argv[6::3]):
+
+        # the first time through, we haven't loaded the data, but we can hold onto handles to them
         if plexos_cxn is None or ptdf_data is None:
             plexos_cxn, plexos_sol = main(from_bus, to_bus, timestamp, ptdf_file, plexos_db=plexos_db, plexos_sol=sol_file)
+
+        # the second and subsequent times through, we already have loaded the data.
         else:
             main(from_bus, to_bus, timestamp, ptdf_file, db = plexos_cxn, sol = plexos_sol)
 
--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 This is a library of Python scripts that can be used with PLEXOS to perform different types of advanced analysis. 
 Some of these analyses leverage the PLEXOS API, which is described in detail in a separate repository.
 Each grouping of scripts should come with some documentation that describes how to run the script and what
-PLEXOS modeling requirements are related to its utilization
+PLEXOS modeling requirements are related to its utilization.
 
 # Support
 
 These scripts are not a supported part of the PLEXOS product. They are provided only as a convenience to users
 who may seek to perform the analyses that are described and formulated herein.
 
-Energy Exemplar is under no obligation to support, maintain, update, or explain these scripts. Any action Energy
+Energy Exemplar is under no obligation to support, maintain, update, or document these scripts. Any action Energy
 Exemplar or its staff may take in regards to any of the preceding is voluntary, but does not imply any future
 obligation.
