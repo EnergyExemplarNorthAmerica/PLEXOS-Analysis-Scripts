82287c261375618097f4b3f8b6544e67db741863
Steven Broad <steven.broad@energyexemplar.com>
2020-04-04 17:36:03 -0700

    Finished NYMEX Updater

---
+++ NYMEX Update/nymex_resimulate.py
@@ -0,0 +1,326 @@
+# for PLEXOS API
+import os, sys, clr
+import subprocess as sp
+
+# get the PLEXOS Power BI Link
+sys.path.append('../Power BI Link')
+import power_bi_link as pbi
+
+# for string parsing
+import re
+
+# for datetime manipulation
+import calendar as cal
+from datetime import date, timedelta
+
+# for web scraping
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+def add_month(dt):
+    dt += timedelta(days = cal.monthrange(dt.year, dt.month)[1])
+    return dt 
+
+def setup_date_mapping():
+    date_from = dict()
+    #dt = DateTime(DateTime.Today.Year, DateTime.Today.Month, 1)
+    dt = DateTime(2024, DateTime.Today.Month, 1)
+    date_from['HenryHub'] = dt
+    date_from['Contract1'] = dt.AddMonths(1)
+    date_from['Contract2'] = dt.AddMonths(2)
+    date_from['Contract3'] = dt.AddMonths(3)
+    date_from['Contract4'] = dt.AddMonths(4)
+    return date_from
+
+def pull_nymex_hh():
+    url = 'https://www.eia.gov/dnav/ng/ng_pri_fut_s1_d.htm'
+    req = Request(url, headers={'User-Agent' : "Magic Browser"}) 
+
+    with urlopen( req ) as con:
+        soup = BeautifulSoup(con, features='html.parser')
+
+    gas_prices = dict()
+    for row in soup.body.find('table', attrs={'class':'data1'}).find_all('tr', attrs={'class':'DataRow'}):
+        try:
+            header = re.sub(r'\s','',row.find('td',attrs={'class':'DataStub'}).text)
+            data = re.sub(r'\s','',row.find('td', attrs={'class':'Current2'}).text)
+            gas_prices[header] = float(data)
+        except:
+            continue
+
+    return gas_prices
+
+def parse_date(string):
+    try:
+        return DateTime.Parse(string)
+    except:
+        return None
+
+def is_object(db, class_id, object_name):
+    try:
+        '''
+        Int32 ObjectName2Id(
+            ClassEnum ClassId,
+            String Name
+            )
+        '''
+        return db.ObjectName2Id(class_id, object_name) > 0
+    except:
+        return False
+
+def create_object(db, class_id, object_name, strCategory = None, strDescription = None):
+    '''
+    Int32 AddObject(
+        String strName,
+        ClassEnum nClassId,
+        Boolean bAddSystemMembership,
+        String strCategory[ = None],
+        String strDescription[ = None]
+        )
+    '''
+    if not is_object(db, class_id, object_name):
+        db.AddObject(object_name, class_id, True, strCategory, strDescription)
+        return True
+    else:
+        return False
+
+def is_membership(db, collection, parent, child):
+    '''
+    Int32 GetMembershipID(
+        CollectionEnum nCollectionId,
+        String strParent,
+        String strChild
+        )
+    '''
+    try:
+        return db.GetMembershipID(collection, parent, child) > 0
+    except:
+        return False
+
+def create_membership(db, collection, parent_class_id, parent, child_class_id, child, one_to_one = False):
+    '''
+    Int32 AddMembership(
+        CollectionEnum nCollectionId,
+        String strParent,
+        String strChild
+        )
+    '''
+
+    # convert the child to a list
+    if type(child) is str:
+        child = [child]
+
+    # if the membership is 1-1, we need to remove any existing items
+    if one_to_one:
+        child = [child[0]]
+        try:
+            existing_list = db.GetChildMembers(collection, parent)
+            existing_list = [] if existing_list is None else existing_list
+            for c in existing_list:
+                db.RemoveMembership(collection, parent, c)        
+        except:
+            pass
+
+    # create the membership if required
+    for c in child:
+        if not is_membership(db, collection, parent, c):
+
+            # make sure the objects exist
+            create_object(db, parent_class_id, parent)
+            create_object(db, child_class_id, c)
+
+            # add the membership
+            db.AddMembership(collection, parent, c)
+        
+    return True
+
+def update_or_add_property(db, value, **params):
+
+    # get membership id ... this is for a particular object
+    mem_id = db.GetMembershipID(params['nCollectionId'], params['strParent'], params['strChild'])
+
+    # add the scenario if it doesn't exist
+    create_object(db, ClassEnum.Scenario, params['Scenario'])
+
+    # delete the property row if it exists
+    db.RemoveProperty(mem_id, params['EnumId'], params['BandId'], \
+            params['DateFrom'], params['DateTo'], params['Variable'], \
+            params['DataFile'], params['Pattern'], params['Scenario'], \
+            params['Action'], PeriodEnum.Interval)
+
+    # add a new property row
+    db.AddProperty(mem_id, params['EnumId'], params['BandId'], \
+            value, params['DateFrom'], params['DateTo'], params['Variable'], \
+            params['DataFile'], params['Pattern'], params['Scenario'], \
+            params['Action'], PeriodEnum.Interval)
+
+def plexos_update_prices(db, gas_fuels, gas_scenario, prices = None):
+    if prices is None: return False
+
+    dates = setup_date_mapping()
+    for k in prices:
+        for f in gas_fuels:
+            update_or_add_property(db, prices[k], \
+                nCollectionId = CollectionEnum.SystemFuels, \
+                strParent = 'System', strChild = f, \
+                EnumId = SystemFuelsEnum.Price, \
+                BandId = 1, \
+                DateFrom = dates[k], \
+                DateTo = None, \
+                Variable = None, \
+                DataFile = None, \
+                Pattern = None, \
+                Scenario = gas_scenario, \
+                Action = '=')
+
+    
+    return True
+
+def adjust_study_horizon(db, horizon_name, start_date, months):
+    '''
+    Update the Horizon object to run a number of days from the start_date
+    '''
+    db.UpdateAttribute(ClassEnum.Horizon, horizon_name, HorizonAttributeEnum.DateFrom, start_date.ToOADate())
+    db.UpdateAttribute(ClassEnum.Horizon, horizon_name, HorizonAttributeEnum.StepType, 3.0)
+    db.UpdateAttribute(ClassEnum.Horizon, horizon_name, HorizonAttributeEnum.StepCount, float(months))
+    db.UpdateAttribute(ClassEnum.Horizon, horizon_name, HorizonAttributeEnum.ChronoDateFrom, start_date.ToOADate())
+    db.UpdateAttribute(ClassEnum.Horizon, horizon_name, HorizonAttributeEnum.ChronoStepType, 2.0)
+    db.UpdateAttribute(ClassEnum.Horizon, horizon_name, HorizonAttributeEnum.ChronoStepCount, start_date.AddMonths(months).ToOADate()-start_date.ToOADate())
+
+def add_basic_outputs(db, report_object):
+
+    # Check Daily and Monthly output
+    db.UpdateAttribute(ClassEnum.Report, report_object, ReportAttributeEnum.OutputResultsbyDay, -1.0)
+    db.UpdateAttribute(ClassEnum.Report, report_object, ReportAttributeEnum.OutputResultsbyMonth, -1.0)
+
+    # a list of the report fields to put in output
+    my_list = [\
+        db.ReportPropertyName2PropertyId('System','Generator','Generators','Generation'),
+        db.ReportPropertyName2PropertyId('System','Generator','Generators','Total Generation Cost'),
+        db.ReportPropertyName2PropertyId('System','Generator','Generators','Fuel Offtake'),
+        db.ReportPropertyName2PropertyId('System','Fuel','Fuels','Offtake'),
+        db.ReportPropertyName2PropertyId('System','Fuel','Fuels','Price'),
+        db.ReportPropertyName2PropertyId('System','Region','Regions','Price'),
+        db.ReportPropertyName2PropertyId('System','Region','Regions','Total System Cost'),
+        db.ReportPropertyName2PropertyId('System','Region','Regions','Load'),
+        db.ReportPropertyName2PropertyId('System','Region','Regions','Generation')
+        ]
+    
+    # enable summary reporting for all of the above
+    for idx in my_list:
+        db.AddReportProperty(report_object, idx, SimulationPhaseEnum.MTSchedule, False, True, False, False)
+
+def plexos_update_project(db, project_name, base_scenarios, gas_scenario):
+    model1 = project_name + '_Base'
+    model2 = '{}_{}'.format(project_name, gas_scenario)
+
+    # link up the project
+    db.RemoveObject(gas_scenario, ClassEnum.Report)
+    db.Close()
+    db.Connection(db.DataSource)
+    create_membership(db, CollectionEnum.ProjectModels, ClassEnum.Project, project_name, ClassEnum.Model, model1)
+    create_membership(db, CollectionEnum.ProjectHorizon, ClassEnum.Project, project_name, ClassEnum.Horizon, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ProjectReport, ClassEnum.Project, project_name, ClassEnum.Report, gas_scenario, one_to_one=True)
+
+    # link up base case
+    create_membership(db, CollectionEnum.ModelHorizon, ClassEnum.Model, model1, ClassEnum.Horizon, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ModelReport, ClassEnum.Model, model1, ClassEnum.Report, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ModelMTSchedule, ClassEnum.Model, model1, ClassEnum.MTSchedule, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ModelScenarios, ClassEnum.Model, model1, ClassEnum.Scenario, base_scenarios)
+
+    # link up update case
+    create_membership(db, CollectionEnum.ProjectModels, ClassEnum.Project, project_name, ClassEnum.Model, model2)
+    create_membership(db, CollectionEnum.ModelHorizon, ClassEnum.Model, model2, ClassEnum.Horizon, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ModelReport, ClassEnum.Model, model2, ClassEnum.Report, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ModelMTSchedule, ClassEnum.Model, model2, ClassEnum.MTSchedule, gas_scenario, one_to_one=True)
+    create_membership(db, CollectionEnum.ModelScenarios, ClassEnum.Model, model2, ClassEnum.Scenario, base_scenarios + [gas_scenario])
+
+    # setup the horizon
+    dt = setup_date_mapping()['HenryHub']
+    adjust_study_horizon(db, gas_scenario, dt, 5.0)
+
+    # setup the report object
+    add_basic_outputs(db, gas_scenario)
+
+    return True
+
+def plexos_update(plexos_db, fuel_objects, base_scenarios, gas_scenario, project_name, prices = None):
+
+    # Connect to the PLEXOS input dataset
+    cxn = DatabaseCore()
+    cxn.DisplayAlerts = False
+    cxn.Connection(plexos_db)
+    cxn.DataSource = plexos_db
+
+    # price updates
+    if plexos_update_prices(cxn, fuel_objects, gas_scenario, prices):
+        print('Updated PLEXOS gas prices')
+    else:
+        print('No update for PLEXOS gas prices')
+
+    # update project and models
+    if plexos_update_project(cxn, project_name, base_scenarios, gas_scenario):
+        print('Updated PLEXOS project and models')
+    else:
+        print('No update to PLEXOS project and models')
+
+    # close and save
+    cxn.Close()
+
+def plexos_launch(plexos_db, project_name):
+    # launch the model on the local desktop
+    # The \n argument is very important because it allows the PLEXOS
+    # engine to terminate after completing the simulation
+    sp.call([os.path.join(DatabaseCore().InstallPath, 'PLEXOS64.exe'), plexos_db, r'\n', r'\p', project_name])
+
+def plexos_process(plexos_db, project_name):
+    sol_file = os.path.join(os.path.dirname(plexos_db), 'Project {} Solution'.format(project_name), 'Project {} Solution.zip'.format(project_name))
+    sol_cxn = Solution()
+    sol_cxn.Connection(sol_file)
+    sol_cxn.DisplayAlerts = False
+
+    # pull data
+    pbi.pull_data(sol_cxn, PeriodEnum.Month, ['', sol_file, '-m'], '-m', '_monthly.csv') # pull month results
+
+def main(plexos_db, fuel_objects, base_scenarios, gas_scenario, project_name):
+
+    #plexos_update(plexos_db, fuel_objects, base_scenarios, gas_scenario, project_name, pull_nymex_hh())
+    #plexos_launch(plexos_db, project_name)
+    plexos_process(plexos_db, project_name)
+
+def parse_cli():
+    cli = dict()
+    last_switch = None
+    for arg in sys.argv[1:]:
+        if arg[0] == '-':
+            cli[arg] = None
+            last_switch = arg
+        elif last_switch is not None:
+            cli[last_switch] = arg
+        else:
+            cli['-'] = arg
+
+    return cli
+
+if __name__ == '__main__':
+    
+    plexos_path = 'C:/Program Files/Energy Exemplar/PLEXOS 8.2/'
+    sys.path.append(plexos_path)
+    clr.AddReference('PLEXOS7_NET.Core')
+    clr.AddReference('EEUTILITY')
+
+    from PLEXOS7_NET.Core import DatabaseCore, Solution, PLEXOSConnect
+    from EEUTILITY.Enums import *
+    from System import DateTime
+    import power_bi_link as pbi
+    pbi.set_plexos_path(plexos_path)
+
+    cli = parse_cli()
+    plexos_db = cli['-d'] if '-d' in cli else 'rtsDEMO/rts_PLEXOS.xml'
+    gas_scenario = cli['-s'] if '-s' in cli else 'NYMEX'
+    fuel_objects = cli['-g'].split(',') if '-g' in cli else ['NG/CC', 'NG/CT']
+    project_name = cli['-m'] if '-m' in cli else '3Month'
+    base_scenarios = cli['b'].split(',') if '-b' in cli else ["Gen Outages","Load: DA","RE: DA","Add Spin Up"]
+
+    main(plexos_db, fuel_objects, base_scenarios, gas_scenario, project_name)
+    
--- Power BI Link/power_bi_link.py
+++ Power BI Link/power_bi_link.py
@@ -3,100 +3,73 @@ import pandas as pd
 import numpy as np
 import io, re, os, sys, time, clr
 
-# load PLEXOS assemblies:
-plexos_path = 'C:/Program Files (x86)/Energy Exemplar/PLEXOS 8.1/'
-sys.path.append(plexos_path)
-clr.AddReference('PLEXOS7_NET.Core')
-clr.AddReference('EEUTILITY')
-
-from PLEXOS7_NET.Core import *
-from EEUTILITY.Enums import *
 from System import Enum, Boolean, DateTime
 
-def is_switch(arg_opt):
+def is_switch(args, arg_opt):
     '''
     Check to see if switch arg_opt is defined in the command line
     '''
-    return arg_opt in sys.argv
+    return arg_opt in args
 
-def switch_index(arg_opt):
+def switch_index(args, arg_opt):
     '''
     Determine the index at which arg_opt occurs
     '''
-    return -1 if not is_switch(arg_opt) else sys.argv.index(arg_opt)
+    return -1 if not is_switch(args, arg_opt) else args.index(arg_opt)
 
-def switch_data(arg_opt):
+def switch_data(args, arg_opt):
     '''
     Find the data if any that is associated with switch arg_opt
     '''
-    if is_switch(arg_opt):
-        idx = switch_index(arg_opt)
-        if 0 <= idx < len(sys.argv) - 1:
-            if not sys.argv[idx + 1][0] == '-':
-                return sys.argv[idx + 1]
+    if is_switch(args, arg_opt):
+        idx = switch_index(args, arg_opt)
+        if 0 <= idx < len(args) - 1:
+            if not args[idx + 1][0] == '-':
+                return args[idx + 1]
     
     return None
 
-def switch_data_to_date(arg_opt):
+def switch_data_to_date(args, arg_opt):
     '''
     Pull a date from the command line
     '''
     try:
-        return DateTime.Parse(switch_data(arg_opt))
+        return DateTime.Parse(switch_data(args, arg_opt))
     except:
         return None
 
-def query_data_to_csv(sol, csv_file, sim_phase, coll, period, date_from, date_to):
+def query_data_to_csv(sol, csv_file, sim_phase, coll, period, date_from, date_to, is_verbose = False):
     # Run the query
-    '''
-    Boolean QueryToCSV(
-        String strCSVFile,
-        Boolean bAppendToFile,
-        SimulationPhaseEnum SimulationPhaseId,
-        CollectionEnum CollectionId,
-        String ParentName,
-        String ChildName,
-        PeriodEnum PeriodTypeId,
-        SeriesTypeEnum SeriesTypeId,
-        String PropertyList[ = None],
-        Object DateFrom[ = None],
-        Object DateTo[ = None],
-        String TimesliceList[ = None],
-        String SampleList[ = None],
-        String ModelName[ = None],
-        AggregationEnum AggregationType[ = None],
-        String Category[ = None],
-        String Separator[ = ,]
-        )
-    '''
+    from EEUTILITY.Enums import SeriesTypeEnum
     params = (csv_file, True, sim_phase, coll, '', '', period, SeriesTypeEnum.Values, '', date_from, date_to)
     try:
         if sol.QueryToCSV(*params):
-            if is_switch('-v'):
+            if is_verbose:
                 print('{1} successfully {0} phase output.'.format(str(sim_phase), str(coll)))
         else:
-            if is_switch('-v'):
+            if is_verbose:
                 print('{1} found no {0} phase output.'.format(str(sim_phase), str(coll)))
     except Exception as ex:
-        if is_switch('-v'):
+        if is_verbose:
             print('{1} is not present in the {0} phase output.'.format(str(sim_phase), str(coll)))
 
 
-def pull_data(sol_cxn, time_res, arg_opt, default_csv):
+def pull_data(sol_cxn, time_res, args, arg_opt, default_csv):
+    from EEUTILITY.Enums import SimulationPhaseEnum, CollectionEnum, PeriodEnum
 
     # is this time_res active? If not quit
-    if not is_switch(arg_opt): return
+    if not is_switch(args, arg_opt): return
 
     # date_from and date_to
-    date_from = switch_data_to_date('-f')
-    date_to = switch_data_to_date('-t')
+    date_from = switch_data_to_date(args, '-f')
+    date_to = switch_data_to_date(args, '-t')
 
     start_time = time.time() # start timer
 
     # get the csv_file name
-    csv_file = switch_data(arg_opt)
+    csv_file = switch_data(args, arg_opt)
     if csv_file is None:
-        csv_file = re.sub('\.zip$', '', sys.argv[1]) + default_csv
+        csv_file = re.sub('\.zip$', '', args[1]) + default_csv
 
     # remove the csv_file if it already exists
     if os.path.exists(csv_file): os.remove(csv_file)
@@ -104,11 +77,13 @@ def pull_data(sol_cxn, time_res, arg_opt, default_csv):
     # loop through all relevant collections and phases
     for phase in Enum.GetValues(clr.GetClrType(SimulationPhaseEnum)):
         for coll in Enum.GetValues(clr.GetClrType(CollectionEnum)):
-            query_data_to_csv(sol_cxn, csv_file, phase, coll, time_res, date_from, date_to)
+            query_data_to_csv(sol_cxn, csv_file, phase, coll, time_res, date_from, date_to, is_verbose=is_switch(args, '-v'))
 
     print('Completed',clr.GetClrType(PeriodEnum).GetEnumName(time_res),'in',time.time() - start_time,'sec')
 
 def main():
+    from EEUTILITY.Enums import PeriodEnum
+    
     if len(sys.argv) <= 1:
         print('''
 Usage:
@@ -133,16 +108,24 @@ Usage:
     sol_cxn.DisplayAlerts = False
 
     # pull data
-    pull_data(sol_cxn, PeriodEnum.FiscalYear, '-y', '_annual.csv') # pull annual results
-    pull_data(sol_cxn, PeriodEnum.Quarter, '-q', '_quarterly.csv') # pull quarter results
-    pull_data(sol_cxn, PeriodEnum.Month, '-m', '_monthly.csv') # pull month results
-    pull_data(sol_cxn, PeriodEnum.Week, '-w', '_weekly.csv') # pull week results
-    pull_data(sol_cxn, PeriodEnum.Day, '-d', '_daily.csv') # pull day results
-    pull_data(sol_cxn, PeriodEnum.Hour, '-h', '_hourly.csv') # pull hourly results
-    pull_data(sol_cxn, PeriodEnum.Interval, '-i', '_interval.csv') # pull interval results
+    pull_data(sol_cxn, PeriodEnum.FiscalYear, sys.argv, '-y', '_annual.csv') # pull annual results
+    pull_data(sol_cxn, PeriodEnum.Quarter, sys.argv, '-q', '_quarterly.csv') # pull quarter results
+    pull_data(sol_cxn, PeriodEnum.Month, sys.argv, '-m', '_monthly.csv') # pull month results
+    pull_data(sol_cxn, PeriodEnum.Week, sys.argv, '-w', '_weekly.csv') # pull week results
+    pull_data(sol_cxn, PeriodEnum.Day, sys.argv, '-d', '_daily.csv') # pull day results
+    pull_data(sol_cxn, PeriodEnum.Hour, sys.argv, '-h', '_hourly.csv') # pull hourly results
+    pull_data(sol_cxn, PeriodEnum.Interval, sys.argv, '-i', '_interval.csv') # pull interval results
 
     print ('Completed in', time.time() - start_time, 'sec')
 
+def set_plexos_path(plexos_path):
+    # load PLEXOS assemblies:
+    sys.path.append(plexos_path)
+    clr.AddReference('PLEXOS7_NET.Core')
+    clr.AddReference('EEUTILITY')
+    from PLEXOS7_NET.Core import DatabaseCore, Solution, PLEXOSConnect
 
 if __name__ == '__main__':
+
+    set_plexos_path('C:/Program Files (x86)/Energy Exemplar/PLEXOS 8.1/')
     main()
